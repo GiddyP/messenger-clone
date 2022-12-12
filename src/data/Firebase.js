8b1fb5965da8676a5dfd2e971fd063164b897806
giddyPwild <giddygbenga4@gmail.com>
2022-12-12 06:38:20 +0100

    second Commit

--- src/data/Firebase.js
+++ src/data/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyADWgayzmvhmzkMcVucKKN4qOLXnnWhbHY",
-    authDomain: "messenger-clone2-a984e.firebaseapp.com",
-    projectId: "messenger-clone2-a984e",
-    storageBucket: "messenger-clone2-a984e.appspot.com",
-    messagingSenderId: "858564569107",
-    appId: "1:858564569107:web:9199577237f6c5d8e277e6",
-    measurementId: "G-50H78ZD8WB"
+    apiKey: "AIzaSyA969XdLNyAfm984NkBupqX525AZk2OIfI",
+    authDomain: "messenger-clone-aa071.firebaseapp.com",
+    projectId: "messenger-clone-aa071",
+    storageBucket: "messenger-clone-aa071.appspot.com",
+    messagingSenderId: "911037533241",
+    appId: "1:911037533241:web:613d27817b34f2de7251e8",
+    measurementId: "G-T55Q9JQL7Y"
 };
 
 
